72f138ef9b8479c6356e29418906a87d4352d2c1
Dezmond2 <dezmond22@gmail.com>
2015-07-12 17:44:51 +0300

    Update

---
+++ Blog.WebUI.Admin/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Blog.Repository;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.WebUI.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public HomeController()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["BlogEntities"].ConnectionString;
+            this._userRepository = new EFUserRepository(connectionString);
+        }
+        //
+        // GET: Home
+        public ActionResult Index()
+        {
+            var users = this._userRepository.GetUsers();
+            ViewBag.Users = users;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UpdateUser(int Id, bool IsEnable)
+        {
+            this._userRepository.UpdateUser(Id, IsEnable);
+            var users = this._userRepository.GetUsers();
+            ViewBag.Users = users;
+            return View("Index");
+        }
+    }
+}
